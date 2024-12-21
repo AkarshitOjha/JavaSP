fcdc58361eadb16f5fa96b1207c67e951048f22f
Akarshit <domain.hub0web@gmail.com>
2024-12-21 06:26:39 +0000

    1.0

---
+++ variables/var.js
@@ -0,0 +1,7 @@
+const customerId = 22345
+let customerName = "Akarshit"
+var city = "hydrabad"
+
+// console.log(customerId)
+
+console.table([customerId , customerName , city])   // this type console show on table format output
