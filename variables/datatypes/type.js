c72023615e6d0b34c72d7d9df3dba44ee045d350
Akarshit <domain.hub0web@gmail.com>
2024-12-21 07:44:15 +0000

    1.1

---
+++ variables/datatypes/type.js
@@ -0,0 +1,26 @@
+// datatype in js
+// In JavaScript, strings are surrounded by quotes:
+
+// Single quotes: 'Hello'
+let singleQuote = 'hello'
+// Double quotes: "Hello"
+let doubleQuote = 'hello1'
+// Backticks: `Hello`
+let backticks = `hello2`
+
+console.table([singleQuote,doubleQuote,backticks])
+
+// JavaScript Number
+// In JavaScript, the number type represents numeric values (both integers and floating-point numbers).
+// Integers - Numeric values without any decimal parts  Infinity, -Infinity , NaN (Not-a-Number) it is a computational problem.
+
+
+
+let int = 949557574
+let int1 = -35
+let int2 = -10000000000000
+
+console.table([int,int1,int2])
+
+
+
