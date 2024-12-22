5ee1a71d01178855da688e9429d5413fd900f772
Akarshit <domain.hub0web@gmail.com>
2024-12-22 05:39:42 +0000

    1.2

--- variables/datatypes/type.js
+++ variables/datatypes/type.js
@@ -1,4 +1,7 @@
 // datatype in js
+
+//  Primitive Data types 
+
 // In JavaScript, strings are surrounded by quotes:
 
 // Single quotes: 'Hello'
@@ -9,11 +12,12 @@ let doubleQuote = 'hello1'
 let backticks = `hello2`
 
 console.table([singleQuote,doubleQuote,backticks])
+// ==========================================================================================================================================
 
 // JavaScript Number
 // In JavaScript, the number type represents numeric values (both integers and floating-point numbers).
-// Integers - Numeric values without any decimal parts  Infinity, -Infinity , NaN (Not-a-Number) it is a computational problem.
-
+// Integers - Numeric values without any decimal parts  Infinity, -Infinity , NaN (Not-a-Number) it is a computational error. It is a result of an incorrect--
+// or an undefined mathematical operation, for instance:
 
 
 let int = 949557574
@@ -22,5 +26,34 @@ let int2 = -10000000000000
 
 console.table([int,int1,int2])
 
+// -----Nan
+console.log( "not a number" / 2 );
+
+// ==============================================================================================================================================
+
+// BigInt use ---------------------------
+
+//  to hndle the large numric data 
+// how to use -----
+
+let x = 12737473n
+let y =  475748847538475n    // (n)-- represent BigInt 
+console.log(x*y)
+
+// ==============================================================================================================================================
+
+// Booleaen return only true false
+// ==============================================================================================================================================
+// Undefined type
+// The meaning of undefined is “value is not assigned”. If a variable is declared, but not assigned, then its value is undefined:
+
+let age;
+console.log(age)
+// ==============================================================================================================================================
+// Null type 
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+
+let age1= null
+console.log(null)
 
 
