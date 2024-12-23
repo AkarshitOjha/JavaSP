5f6d24866dc48b8f6d21dce575cfd67232b9cbc8
Akarshit <domain.hub0web@gmail.com>
2024-12-23 04:42:31 +0000

    1.0

---
+++ variables/datatypes/typeconversion/conver.js
@@ -0,0 +1,19 @@
+//let x = 100
+//let whatisx = Number (x)
+//console.log(typeof(whatisx))    // give output --> Number
+//console.log(typeof(x))         // give output --> Number
+
+//let y = "100"
+//let whatisy = Number (y)
+//console.log(typeof(whatisy))  // give --> Number
+//console.log(typeof(y))        // give -->string
+
+
+let z = "100"
+let whatisz = Number (z)
+console.log(typeof(whatisz/"2"))  // give --> Number
+console.log(typeof(z/2))  // give-> Number
+
+let a = 235
+let b = "akarshit"
+console.log(a*b) // giva --> NaN
