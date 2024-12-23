3af9677814d1a9e1259be2849c63655e5b09139c
Akarshit <domain.hub0web@gmail.com>
2024-12-23 06:25:50 +0000

    1.0

---
+++ stringAdd/add.js
@@ -0,0 +1,11 @@
+let a = "hello"
+let b = " akrshit"
+// console.log(a+b)   // give--> hello akarshit
+
+let d = "hi"
+let c = 4
+// console.log(d+c) // give--> hi4
+
+let f = 6
+let y = "hi"
+console.log
---
+++ variables/strad/strr.js
@@ -0,0 +1,21 @@
+let a = 9
+let b ="10"
+let c = a+b
+console.log(c)   // --> 910
+
+let ab = "10"
+let ac = 8
+let ad = ab+ac
+console.log(ad)    // --> 108
+
+let q = "hello"
+let r = " akarshit"
+console.log(q+r)
+
+let s = 10
+let ss = "hi"
+console.log(s+s+ss)  // --> 20hi
+
+let tt = 10
+let yy = "hello"
+console.log(yy+tt+tt)  //-->hello1010
