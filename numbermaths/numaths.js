9398adc6671ac6b94df51cd0c1797e99e8e31d58
Akarshit <domain.hub0web@gmail.com>
2024-12-25 06:02:11 +0000

    1.0

---
+++ numbermaths/numaths.js
@@ -0,0 +1,40 @@
+let ab = new Number(100)
+console.log(ab);
+console.log(ab.toFixed(2));
+
+
+let bv = new Number(100)
+console.log(bv.toPrecision(4))  
+
+const rupee = 120099000
+console.log(rupee.toLocaleString('en-IN'));
+
+
+// ==============================================================================================================================
+
+// --------------------------------------------------------Maths-------------------------------------------------------
+// ------------------------ Maths library Java Script ke saath aati hai by default-------------------------------------
+
+console.log(Math)
+console.log(Math.abs(-5));   // abs conver negative to positive value
+console.log(Math.round(5.7));   // round convert value in roundoff type 
+console.log(Math.ceil(7.1));   // ceil give top value mean agar point ke uper koi bhi value hai to high value hi dega 
+console.log(Math.floor(9.5));  // floor give always lower value . ke baad koi bhi value hogi use count nikrega 
+console.log(Math.max(4,6,33));  // always give only max value
+console.log(Math.min(6,33,5,8));  // only min value
+// ===================================================================================================
+// random 
+
+console.log(Math.random())          //  random give value in between 0 to 1 
+
+console.log(Math.random()*10);
+
+
+
+
+
+
+
+
+
+
