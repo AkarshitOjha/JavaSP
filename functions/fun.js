28d62f3cff67852ddc08bf923ef1f7768be3b34a
Akarshit <domain.hub0web@gmail.com>
2024-12-30 11:58:57 +0000

    1.1

--- functions/fun.js
+++ functions/fun.js
@@ -31,13 +31,40 @@ function addtocart (...cart){
 }
 
 let cart=(addtocart(500 ,1000,455,678,900));   // iska ye mtlb tha kii hamre cart me user kitni value lega to ni pata isliye hum (...cart)->rest operator use krenge
-console.log("Your cart ",cart);
+// console.log("Your cart ",cart);
 
 
 // object function me kaise paas hoga  ===============================================================================================
 
 // create object 
-const 
+const mypersonaldetail = {
+    name:"Akarshit ",
+    lastName:"ojha",
+    conatact:8545025990,
+    loacation:"Hyderabad",
+    age:21
+}
+
+// create function and pass it 
+function  mydetail(detail){
+    let det = (`my personal deatil is -${detail.name + detail.lastName} , my contact-${detail.conatact},my loaction ${detail.loacation},my age -${detail.age}`)  
+    return det
+}
+let det =(mydetail(mypersonaldetail));
+// console.log(det)
+
+// same as create array and pass in function
+
+const ar1 = [1,23,4,5]
+ 
+function myar1 (aar){
+    let ww = aar
+    return ww
+}
+let ww = myar1(ar1)
+console.log("My array is =",ww)
+
+
 
 
 
