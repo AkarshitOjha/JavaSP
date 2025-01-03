5bdc645c26223a2c4340b07de5fdaae53f06f611
Akarshit <domain.hub0web@gmail.com>
2025-01-03 13:24:17 +0000

    1.0

---
+++ controlStatement/whiledowhile.js
@@ -0,0 +1,22 @@
+// now we understand while loop 
+
+// let numberr = 1
+// while (numberr<=10) {
+//     if (numberr===4) {
+//         console.log(`here number is found-${numberr} so now break it `);
+//         break;
+//     }
+//     console.log(`here the nuber is ${numberr}`);
+//     numberr++
+    
+// }
+
+//  do while =======================================================================================
+
+// let num = 1
+// do {
+//     console.log(`it is upper num - ${num}`);
+//     num++
+// } while (num<=10);
+
+
