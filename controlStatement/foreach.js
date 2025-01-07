8c01b0c50b136115c6d85d75c9cb943a1c2e7eb1
Akarshit <domain.hub0web@gmail.com>
2025-01-07 17:30:44 +0000

    1.0

---
+++ controlStatement/foreach.js
@@ -0,0 +1,32 @@
+const arr = ["hello" , "akarshit" , "good"]
+
+arr.forEach(function(arrValue){
+    // console.log(arrValue);               // for each me hum function name ni dete
+    
+});
+
+const a1 = [
+    {
+        name:"Akarshit",
+        location:"Hyderabad",
+        age:23,
+        id:34554
+    },
+    {
+        name1:"Akarshit1",
+        location1:"Hyderabad1",
+        age:24,
+        id1:34554
+    },
+    {
+        name2:"Akarshit",
+        location22:"Hyderabad",
+        age:25,
+        id2:34554
+    },
+]
+
+a1.forEach((items)=>{
+        console.log(items.age);
+        
+})
